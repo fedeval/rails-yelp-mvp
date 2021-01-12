4315b1001ebc1d17765e8b274164cb0f9b0653ba
Federico Valenza <federico.valenza5@gmail.com>
2021-01-12 21:06:17 +0100

    created restaurant user stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning db'
+Restaurant.destroy_all
+puts 'db clean'
+puts 'generating seeds...'
+20.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "generated restaurant #{restaurant.id}"
+end
